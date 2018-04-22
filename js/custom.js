265656cdd7edab824f528eca9867ffb7bbb5f799
mohammed yusuf <mwaigalo5@gmail.com>
2018-04-22 11:42:54 +0300

    b

--- js/custom.js
+++ js/custom.js
@@ -1,19 +1,19 @@
-var x=prompt("Enter your name");
-document.write("hi " + x);
-//function sad(){
-//    var age=prompt("Enter your age");
-//    if(age<10){
-//        Document.write("more than " + age + "go to nursery");
-//        
-//    }
-//    else if(age>10){
-//        document.write("more than"  + age + "go to primary");
-//    }
-//    else if(age<15){
-//        document.write("more than"  + age + "go to secondary");
-//    }
-//    else{
-//        document.write("more than" + age + "go to university");
-//    }
-//}
-//sad()
+//var x=prompt("Enter your name");
+//document.write("hi " + x);
+function sad(){
+    var age=prompt("Enter your age");
+    if(age<10){
+        Document.write("more than " + age + "go to nursery");
+        
+    }
+    else if(age>10){
+        document.write("more than"  + age + "go to primary");
+    }
+    else if(age<15){
+        document.write("more than"  + age + "go to secondary");
+    }
+    else{
+        document.write("more than" + age + "go to university");
+    }
+}
+sad()
