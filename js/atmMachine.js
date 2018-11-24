8de6e66912b45c0e747e404f14c1c039c3550555
mwaiyusuf <mwaigalo5@gmail.com>
2018-11-24 09:18:13 +0300

    t

--- js/atmMachine.js
+++ js/atmMachine.js
@@ -7,8 +7,24 @@ function atm(){
             var proceed=prompt("Enter the number");
             balance=2000;
             if(proceed==1){
-                alert("")
+                alert("Your balance is " + balance);
             }
+            else if(proceed==2){
+                var withdraw=prompt("Enter the amount you want to withdraw");
+                bal=balance-withdraw;
+                if(withdraw>=balance){
+                    alert("your amount is insufficient");
+                }
+                else{
+                    alert("you have withdrawn " + withdraw + " adn your balance is " + bal);
+                }
+            }
+            else if(proceed==3){
+                var deposit=prompt("enter the amount you want to deposit");
+                dep=deposit+balance;
+                alert("you have deposited " + dep);
+            }
+            break;
         }
         else{
             alert("Sorry, you have " + i + " attempts left");
